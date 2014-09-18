a0577cc49804fa1feda07d17b680930b8b3d0ad1
Chibisi Chima-Okereke <chibisi@activeanalytics.co.uk>
2014-09-18 01:08:29 +0100

    Create rda.r

---
+++ rda.r
@@ -0,0 +1,9 @@
+# Benchmarks for reading/writing from/to rda (RData) files
+
+# Writing
+write_file <- "../data/air_2007.rda"
+system.time(save(dat, file = write_file))
+
+
+# Reading
+system.time(load(file = write_file))
